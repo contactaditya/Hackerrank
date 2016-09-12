90035b528ac3dcf309db4a56739b8e42c1722b0f
contactaditya <contactadityag@gmail.com>
2016-09-12 01:53:58 -0400

    This is the solution to the Hackerrank Problem Staircase.

---
+++ Algorithms/Warmup/Staircase.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+
+public class Staircase {
+
+ public static void main(String[] args) {        
+  Scanner input = new Scanner(System.in);
+  System.out.print("Enter the size of the staircase: ");
+  int n = input.nextInt();
+  System.out.println();  
+  for(int i=1; i<=n; i++) {
+   for(int j=1; j<=n; j++) {
+     if((i + j) > n) {
+       System.out.print("#");
+     }
+     else {
+       System.out.print(" ");
+     }           
+   }
+        
+  System.out.println();   
+  }
+ }
+}
