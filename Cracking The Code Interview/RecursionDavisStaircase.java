0af0d55b5f56444ebb0ffe9f21673580a4a7be8b
contactaditya <contactadityag@gmail.com>
2016-10-21 20:20:13 -0400

    This is the solution to the Hackerrank Problem Recursion: Davis' Staircase.

---
+++ Cracking The Code Interview/RecursionDavisStaircase.java
@@ -0,0 +1,97 @@
+import java.util.*;
+
+public class RecursionDavisStaircase {
+	
+ public static int countPathsR(int steps) {
+   if(steps < 0) {
+    return 0;
+   }
+        
+   else if(steps == 0) {
+    return 1;  
+   }
+            
+  return countPathsR(steps - 1) + countPathsR(steps - 2) + countPathsR(steps - 3);
+ }
+      
+  public static int countPathsMemo(int steps) {
+    if(steps < 0) {
+     return 0;
+    }
+          
+   return countPathsMemo(steps, new int[steps + 1]);     
+  }
+      
+  public static int countPathsMemo(int steps, int[] memo) {
+    if(steps < 0) {
+      return 0;
+    }
+        
+    else if(steps == 0) {
+      return 1;  
+    }
+          
+    if(memo[steps] == 0) {           
+      memo[steps] = countPathsMemo(steps - 1, memo) + countPathsMemo(steps - 2, memo) + countPathsMemo(steps - 3, memo);   
+    }
+            
+  return memo[steps];
+ }
+      
+ public static int countPathsDP(int steps) {
+   if(steps < 0) {
+     return 0;
+   }
+        
+   else if(steps <= 1) {
+     return 1;  
+   }
+          
+   int paths[] = new int[steps + 1];
+   paths[0] = 1;
+   paths[1] = 1;  
+   paths[2] = 2; 
+          
+   for(int i=3; i<=steps; i++) {         
+    paths[i] = paths[i - 1] + paths[i - 2] + paths[i - 3];   
+   }
+            
+  return paths[steps];
+ }
+
+ public static int countPathsI(int steps) {
+  if(steps < 0) {
+    return 0;
+  }
+        
+  else if(steps <= 1) {
+    return 1;  
+  }
+          
+  int paths[] = {1, 1, 2};
+          
+  for(int i=3; i<=steps; i++) {         
+   int count = paths[2] + paths[1] + paths[0];   
+   paths[0] = paths[1];
+   paths[1] = paths[2];
+   paths[2] = count;
+  }
+            
+  return paths[2];
+ }
+
+ public static void main(String[] args) {
+  Scanner input = new Scanner(System.in);
+  System.out.print("Enter number of staircases in the houses: ");
+  int s = input.nextInt();
+  
+  for(int i = 0; i < s; i++) {
+   System.out.println();
+   System.out.print("Enter the height of the staircases: ");
+   int steps = input.nextInt();
+   System.out.println();
+   System.out.println("The number of ways to climb the staircase: " + countPathsMemo(steps));
+  }
+  input.close();
+ }
+}
