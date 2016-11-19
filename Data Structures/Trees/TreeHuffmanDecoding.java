f842d9889334eb9d374555902f5e7ecae42510d5
contactaditya <contactadityag@gmail.com>
2016-11-18 22:35:01 -0500

    This is the solution to the Hackerrank Problem: Tree: Huffman Decoding.

---
+++ Data Structures/Trees/TreeHuffmanDecoding.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+ class NodeHuffman {
+   char data;
+   int frequency;
+   NodeHuffman left;
+   NodeHuffman right;
+   
+   public NodeHuffman(char data, int frequency) {
+	 this.data = data;
+	 this.frequency = frequency;
+	 left = right = null;
+   }
+  } 
+ 
+ public class TreeHuffmanDecoding {
+	 
+  void decode(String S ,Node root) {
+    StringBuilder sb = new StringBuilder();
+    Node c = root;
+    for (int i = 0; i < S.length(); i++) {
+	  c = S.charAt(i) == '1' ? c.right : c.left;
+	  if (c.data!='\0') {
+	    sb.append(c.data);
+		c = root;
+	  }
+	}
+   System.out.print(sb);
+  }
+
+  public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+  }
+}
