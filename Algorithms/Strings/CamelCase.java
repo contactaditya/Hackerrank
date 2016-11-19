dc667f1bef4b8e14d9fe37bac4a6f56656da8ede
contactaditya <contactadityag@gmail.com>
2016-11-18 22:42:48 -0500

    This is the solution to the Hackerrank Problem: CamelCase.

---
+++ Algorithms/Strings/CamelCase.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+ public class CamelCase {
+
+ public static void CamelCase(String s) {
+   int count = 1;
+   for (int i = 0; i < s.length(); i++) {
+	 if(Character.isUpperCase(s.charAt(i))) {
+	    count++; 
+	 }
+   } 
+  System.out.println(); 
+  System.out.print("The number of words in the String " + s + " is: " + count); 
+  }
+
+ public static void main(String[] args) {
+   Scanner in = new Scanner(System.in);
+   System.out.print("Enter a string in camel case: ");
+   String s = in.next();
+   CamelCase(s);
+  }
+ }
