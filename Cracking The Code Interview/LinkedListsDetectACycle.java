a62a999a7c80dd62e60c5acab195a70e931b08d3
contactaditya <contactadityag@gmail.com>
2016-11-21 18:51:17 -0500

    This is the solution to the Hackerrank Problem: Linked Lists: Detect a cycle.

--- Cracking The Code Interview/LinkedListsDetectACycle.java
+++ Cracking The Code Interview/LinkedListsDetectACycle.java
@@ -15,7 +15,23 @@ class LinkedListNode {
    this.data = data;
    this.next = next;
  }
-}
+ 
+ public Object getData() {
+   return data;
+ }
+		
+ public void setData(Object data) {
+   this.data = data; 
+ }
+		
+ public LinkedListNode getNext() {
+    return next;
+ }
+		
+ public void setNext(LinkedListNode next) {
+	this.next = next;
+  }
+ }
 
 public class LinkedListsDetectACycle {
 	
