4345f365b447e443069dc9b1ab5203839c72d7b0
Aditya Gupta <contactadityag@gmail.com>
2016-12-13 17:57:25 -0500

    This is the solution to the Hackerrank Problem: Cycle Detection.

--- Data Structures/Linked Lists/CycleDetection.java
+++ Data Structures/Linked Lists/CycleDetection.java
@@ -1,17 +1,17 @@
 import java.util.*;
 
 public class CycleDetection { 	
-	private static LinkedListNode head;
-	private static int size;  
+    private static LinkedListNode head;
+    private static int size;  
 
-	public CycleDetection() {
+   public CycleDetection() {
    // this is an empty list, so the reference to the head node is set to a new node with no data				  
-	   head = new LinkedListNode(null);
-       size = 0;
-	}
+      head = new LinkedListNode(null);
+      size = 0;
+   }
 
-	public static boolean hasCycle(LinkedListNode head) {
-	  LinkedListNode fast = head;
+   public static boolean hasCycle(LinkedListNode head) {
+      LinkedListNode fast = head;
       LinkedListNode slow = head;
      	         		     
       while(fast != null && fast.next != null && slow != null) {
@@ -25,22 +25,22 @@ public class CycleDetection {
   return false;   
   }
 
-	public static void main(String[] args) {  
-	  CycleDetection list = new CycleDetection();
-	  list.head = new LinkedListNode(50);
-	  list.head.next = new LinkedListNode(20);
-	  list.head.next.next = new LinkedListNode(15);
-	  list.head.next.next.next = new LinkedListNode(4);
-	  list.head.next.next.next.next = new LinkedListNode(10);
+   public static void main(String[] args) {  
+        CycleDetection list = new CycleDetection();
+	list.head = new LinkedListNode(50);
+	list.head.next = new LinkedListNode(20);
+	list.head.next.next = new LinkedListNode(15);
+	list.head.next.next.next = new LinkedListNode(4);
+	list.head.next.next.next.next = new LinkedListNode(10);
 	     	     
-	  boolean IsACycle = list.hasCycle(head);
+	boolean IsACycle = list.hasCycle(head);
 	  
-	  if(IsACycle) {
-		System.out.println("There is a cycle in the linkedlist.");  
-	  }
-	  else {
-	    System.out.println("There is no cycle in the linkedlist.");    
-	  }
-	 }
+	if(IsACycle) {
+	   System.out.println("There is a cycle in the linkedlist.");  
+	}
+	else {
+	   System.out.println("There is no cycle in the linkedlist.");    
+	}
+      }
     }
 
