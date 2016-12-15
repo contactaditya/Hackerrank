ded6896e0c80e196e09ffcf3f96a5c8917ea3508
contactaditya <contactadityag@gmail.com>
2016-12-15 13:55:16 -0500

    This is the solution to the Hackerrank Problem: Balanced Brackets.

---
+++ Data Structures/Stacks/BalancedBrackets.java
@@ -0,0 +1,56 @@
+import java.util.*;
+
+ public class BalancedBrackets { 
+    public static char[][] TOKENS = {{'{', '}'}, {'[', ']'}, {'(', ')'}};
+    
+    public static boolean isOpenTerm(char c) {
+      for (char[] array: TOKENS) {
+    	if(array[0] == c) {
+    	  return true;	
+    	}  
+      }	
+     return false;
+    }  
+    
+    public static boolean matches(char openTerm, char closeTerm) {
+      for (char[] array: TOKENS) {
+        if(array[0] == openTerm) {
+    	  return array[1] == closeTerm;	
+        }  
+      }	
+     return false;
+    }
+    
+    public static boolean isBalanced(String expression) {
+      Stack<Character> stack = new Stack<Character>();
+      for(char c : expression.toCharArray()) {
+    	if(isOpenTerm(c)) {
+    	   stack.push(c);	  
+    	}
+    	else {
+    	  if(stack.isEmpty() || !matches(stack.pop(), c)) {
+    		return false;
+    	  }
+    	}
+      }
+     return stack.isEmpty();
+    }
+
+	public static void main(String[] args) {  
+	  Scanner input = new Scanner(System.in);
+	  String expression = new String();	
+	  System.out.print("Please enter an expression with only these characters (){}[]: ");
+	  expression = input.next();  
+	  
+	  boolean result = isBalanced(expression);
+	  
+	  if(result == true) {
+	   System.out.println();
+	   System.out.println("The expression " + expression + " is a balanced expression.");
+	  }
+	  else {
+	   System.out.println();
+	   System.out.println("The expression " + expression + " is not a balanced expression.");	  
+	  }
+	}
+  }
