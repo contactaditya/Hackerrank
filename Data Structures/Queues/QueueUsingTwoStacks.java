4e635a53731954d0cccb2c3365ca95b4f5a3cc02
contactaditya <contactadityag@gmail.com>
2017-03-31 02:27:47 -0400

    This is the solution to the Hackerrank Problem: Queue using Two Stacks.

---
+++ Data Structures/Queues/QueueUsingTwoStacks.java
@@ -0,0 +1,59 @@
+import java.util.*;
+import java.io.*;
+
+public class QueueUsingTwoStacks { 
+	
+  public static class MyQueue<T> {
+	 
+ private Stack<T> stackNewestOnTop = new Stack<T>();
+ private Stack<T> stackOldestOnTop = new Stack<T>();
+
+ public void enqueue(T value) { // Add item
+   stackNewestOnTop.push(value); 
+ }
+
+ public T peek() { // Get "oldest" item
+   shiftStacks();
+   return stackOldestOnTop.peek();  
+ }
+
+ public T dequeue() { // Get "oldest" item and remove it
+   shiftStacks(); 
+   return stackOldestOnTop.pop();	 	 
+ }
+
+ private void shiftStacks() {
+  if(stackOldestOnTop.isEmpty()) {
+	while(!stackNewestOnTop.isEmpty()) {
+	  stackOldestOnTop.push(stackNewestOnTop.pop());	
+	}
+   }
+ }
+}
+		
+ public static void main(String[] args) {  
+	 
+   MyQueue<Integer> queue = new MyQueue<Integer>();
+
+   Scanner scan = new Scanner(System.in);
+   System.out.print("Enter number of operations: ");
+   int n = scan.nextInt();
+
+   for (int i = 0; i < n; i++) {
+	System.out.println();	 
+	System.out.print("Enter the query type: ");	 
+	int operation = scan.nextInt();
+	if (operation == 1) { // enqueue
+	  System.out.println();	  
+	  System.out.print("Enter a element to insert in the queue: "); 	
+	  queue.enqueue(scan.nextInt());
+	} else if (operation == 2) { // dequeue
+	  queue.dequeue();
+	} else if (operation == 3) { // print/peek
+	  System.out.println();	 
+	  System.out.println("The element at the front of the queue is: " + queue.peek());
+	}
+  }
+  scan.close();
+ }
+}
