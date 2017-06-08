4bdbe9ba0ea703755948df8a607c0d2af5af1e11
Aditya Gupta <contactadityag@gmail.com>
2017-06-07 23:49:18 -0400

    This is the solution to the Hackerrank Problem: Tree: Top View.

--- Data Structures/Trees/TreeTopView.java
+++ Data Structures/Trees/TreeTopView.java
@@ -6,20 +6,20 @@ import java.util.*;
     
   public void printgoLeft(Node node) {
 		 
-   if (node.left != null) {
+    if (node.left != null) {
       printgoLeft(node.left);
-   }
+    }
             
-   System.out.print(node.data + " ");		  
+    System.out.print(node.data + " ");		  
   }   
 
   public void printgoRight(Node node) {
 	
-	System.out.print(node.data + " ");		   
+    System.out.print(node.data + " ");		   
 		 
     if (node.right != null) {
-	   printgoRight(node.right);
-	} 
+      printgoRight(node.right);
+    } 
   }     
 
   public void printTopView(Node root) {
@@ -36,18 +36,18 @@ import java.util.*;
   }    
 	
   public static void main(String[] args) { 
-	TreeTopView tree = new TreeTopView();
-	tree.root = new Node(3);
-	tree.root.left = new Node(5);
-	tree.root.right = new Node(2);
-	tree.root.left.left = new Node(1);
-	tree.root.left.right = new Node(4);
-	tree.root.left.left.right = new Node(9);
-	tree.root.right.left = new Node(6);
-	tree.root.right.right = new Node(7);
-	tree.root.right.right.left = new Node(8);
+     TreeTopView tree = new TreeTopView();
+     tree.root = new Node(3);
+     tree.root.left = new Node(5);
+     tree.root.right = new Node(2);
+     tree.root.left.left = new Node(1); 
+     tree.root.left.right = new Node(4);
+     tree.root.left.left.right = new Node(9);
+     tree.root.right.left = new Node(6);
+     tree.root.right.right = new Node(7);
+     tree.root.right.right.left = new Node(8);
 	     
-	System.out.print("Top view of the binary tree is: ");
-	tree.printTopView(tree.root);		
+     System.out.print("Top view of the binary tree is: ");
+     tree.printTopView(tree.root);		
   }
 }
