75561b8c855f4875834ec73041809cdfac1f5c8c
Aditya Gupta <contactadityag@gmail.com>
2017-06-15 03:26:53 -0400

    This is the solution to the Hackerrank Problem: Delete a Node.

--- Data Structures/Linked Lists/DeleteANode.java
+++ Data Structures/Linked Lists/DeleteANode.java
@@ -5,42 +5,42 @@ import java.util.*;
   private static int size;      
  
   public DeleteANode() {
-	  // this is an empty list, so the reference to the head node is set to a new node with no data				  
-	  head = new LinkedListNode(null);
-	  size = 0;
+     // this is an empty list, so the reference to the head node is set to a new node with no data				  
+     head = new LinkedListNode(null);
+     size = 0;
   }
  
   public static LinkedListNode Delete(LinkedListNode head, int position) {   
 	
-	LinkedListNode current = head;
+      LinkedListNode current = head;
 	
-	 if (position == 0){
-	   return current.next;
-	 }  
+      if (position == 0){
+	 return current.next;
+      }  
 	    
-	for(int i=1; i < position; i++) {
-      if(current.getNext() == null) {
-       return head;
-  	  }
-      current = current.getNext();
-    }	 		  
-    System.out.println();
-    System.out.println("The linked list has successfully removed " + current.getNext().getData());
-    current.setNext(current.getNext().getNext());
-    return head;
+      for(int i=1; i < position; i++) {
+        if(current.getNext() == null) {
+          return head;
+  	}
+        current = current.getNext();
+      }	 		  
+      System.out.println();
+      System.out.println("The linked list has successfully removed " + current.getNext().getData());
+      current.setNext(current.getNext().getNext());
+      return head;
   } 
 	
-	public static void main(String[] args) {       
-	  DeleteANode list = new DeleteANode();
-	  list.head = new LinkedListNode(50);
-	  list.head.next = new LinkedListNode(20);
-	  list.head.next.next = new LinkedListNode(15);
-	  list.head.next.next.next = new LinkedListNode(4);
-	  list.head.next.next.next.next = new LinkedListNode(10);
-	  Scanner input = new Scanner(System.in);
-	  System.out.print("Enter the position of the node which you want to insert in the linked list: ");
-	  int position = input.nextInt();  
+ public static void main(String[] args) {       
+     DeleteANode list = new DeleteANode();
+     list.head = new LinkedListNode(50);
+     list.head.next = new LinkedListNode(20);
+     list.head.next.next = new LinkedListNode(15);
+     list.head.next.next.next = new LinkedListNode(4);
+     list.head.next.next.next.next = new LinkedListNode(10);
+     Scanner input = new Scanner(System.in);
+     System.out.print("Enter the position of the node which you want to insert in the linked list: ");
+     int position = input.nextInt();  
 	  
-	  list.Delete(head, position);
+     list.Delete(head, position);
   }
 }
