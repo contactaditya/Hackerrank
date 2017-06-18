08bad82ace84f2aea05d706fcc36b10b0cd20b28
Aditya Gupta <contactadityag@gmail.com>
2017-06-17 22:48:31 -0400

    This is the solution to the Hackerrank Problem Trees: Is This a Binary Search Tree?.

--- Cracking The Code Interview/IsThisABinarySearchTree.java
+++ Cracking The Code Interview/IsThisABinarySearchTree.java
@@ -5,49 +5,49 @@ import java.util.*;
    Node left;
    Node right;
    
-  public Node(int item) {
-   data = item;
-   left = right = null;
-  }
+   public Node(int item) {
+     data = item;
+     left = right = null;
+   } 
  }
  
  public class IsThisABinarySearchTree {
 	
- Node root;	    
- Node previous;
+   Node root;	    
+   Node previous;
   
- boolean checkBST() {	 
-   return checkBST(root, Integer.MIN_VALUE, Integer.MAX_VALUE); 
- }
+   boolean checkBST() {	 
+     return checkBST(root, Integer.MIN_VALUE, Integer.MAX_VALUE); 
+   }
  
- boolean checkBST(Node node, int min, int max) {
+   boolean checkBST(Node node, int min, int max) {
 	 
- if (node == null) {
-   return true;
- }
+      if (node == null) {
+        return true;
+      }
   
-  if (node.data < min || node.data > max) {
+      if (node.data < min || node.data > max) {
 	return false; 
-  }
+      }
     
-  return (checkBST(node.left, min, node.data-1) && checkBST(node.right, node.data+1, max));  
+   return (checkBST(node.left, min, node.data-1) && checkBST(node.right, node.data+1, max));  
  }
 	
  public static void main(String[] args) {
 	 
-  IsThisABinarySearchTree tree = new IsThisABinarySearchTree();
-  tree.root = new Node(4);
-  tree.root.left = new Node(2);
-  tree.root.right = new Node(5);
-  tree.root.left.left = new Node(1);
-  tree.root.left.right = new Node(3);
+    IsThisABinarySearchTree tree = new IsThisABinarySearchTree();
+    tree.root = new Node(4);
+    tree.root.left = new Node(2);
+    tree.root.right = new Node(5);
+    tree.root.left.left = new Node(1);
+    tree.root.left.right = new Node(3);
      
-  if (tree.checkBST()) {
-    System.out.println("This tree is a BST");
-  }
-  else {
-    System.out.println("This tree is not a BST");
-  }
+    if (tree.checkBST()) {
+      System.out.println("This tree is a BST");
+    }
+    else {
+      System.out.println("This tree is not a BST");
+    }
 
- }
+  }
 }
