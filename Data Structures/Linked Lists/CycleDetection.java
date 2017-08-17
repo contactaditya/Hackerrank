c28e38792bb5f83ce16abf68fcdf166cfbd8e485
Aditya Gupta <contactadityag@gmail.com>
2017-08-17 00:36:54 -0400

    This is the solution to the Hackerrank Problem: Cycle Detection.

--- Data Structures/Linked Lists/CycleDetection.java
+++ Data Structures/Linked Lists/CycleDetection.java
@@ -5,7 +5,7 @@ public class CycleDetection {
     private static int size;  
 
    public CycleDetection() {
-   // this is an empty list, so the reference to the head node is set to a new node with no data				  
+     // this is an empty list, so the reference to the head node is set to a new node with no data				  
       head = new LinkedListNode(null);
       size = 0;
    }
@@ -21,26 +21,26 @@ public class CycleDetection {
     	if(slow == fast) {
     	  return true; 
     	}
-     }   	  
-  return false;   
-  }
+      }   	  
+     return false;   
+   }
 
    public static void main(String[] args) {  
-        CycleDetection list = new CycleDetection();
-	list.head = new LinkedListNode(50);
-	list.head.next = new LinkedListNode(20);
-	list.head.next.next = new LinkedListNode(15);
-	list.head.next.next.next = new LinkedListNode(4);
-	list.head.next.next.next.next = new LinkedListNode(10);
+      CycleDetection list = new CycleDetection();
+      list.head = new LinkedListNode(50);
+      list.head.next = new LinkedListNode(20);
+      list.head.next.next = new LinkedListNode(15);
+      list.head.next.next.next = new LinkedListNode(4);
+      list.head.next.next.next.next = new LinkedListNode(10);
 	     	     
-	boolean IsACycle = list.hasCycle(head);
+      boolean IsACycle = list.hasCycle(head);
 	  
-	if(IsACycle) {
-	   System.out.println("There is a cycle in the linkedlist.");  
-	}
-	else {
-	   System.out.println("There is no cycle in the linkedlist.");    
-	}
+      if(IsACycle) {
+	System.out.println("There is a cycle in the linkedlist.");  
       }
-    }
+      else {
+	System.out.println("There is no cycle in the linkedlist.");    
+      }
+   }
+ }
 
