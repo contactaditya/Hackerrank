bbd5f097a8ba8bd0fc662f0ecf991f2810498fae
Aditya Gupta <contactadityag@gmail.com>
2017-08-31 18:25:16 -0400

    This is the solution to the Hackerrank Problem: The Longest Increasing Subsequence.

--- Algorithms/Dynamic Programming/TheLongestIncreasingSubsequence.java
+++ Algorithms/Dynamic Programming/TheLongestIncreasingSubsequence.java
@@ -4,48 +4,48 @@ import java.io.*;
  public class TheLongestIncreasingSubsequence { 
 	
    private static int lengthOfLIS(int[] array) {
-	 ArrayList<Integer> list = new ArrayList<Integer>();   
-	 if(array == null || array.length == 0) {
-	   return 0;
-	 }
+      ArrayList<Integer> list = new ArrayList<Integer>();   
+      if(array == null || array.length == 0) {
+	 return 0;
+      }
 	 
-	 for(int number : array) {
-	   if(list.size() == 0 || number > list.get(list.size()-1)) {
-	      list.add(number);
-	   } else {
-		  int start = 0;
-		  int end = list.size() - 1; 
+      for(int number : array) {
+	if(list.size() == 0 || number > list.get(list.size()-1)) {
+	  list.add(number);
+	} else {
+          int start = 0;
+          int end = list.size() - 1; 
 		  
-		  while(start < end) {
-			int mid = (start + end) / 2;
-			if(list.get(mid) < number) {
-			   start = mid + 1;	
-			} else {
-			   end = mid;	
-			}		  
-		  }
-		  list.set(end, number);  
-	   }
-	 }
+	  while(start < end) {
+	    int mid = (start + end) / 2;
+	    if(list.get(mid) < number) {
+	      start = mid + 1;	
+	    } else {
+	      end = mid;	
+	    }		  
+	  }
+          list.set(end, number);  
+	}
+      }
 	   
-    return list.size(); 
+     return list.size(); 
    }
 
-	public static void main(String[] args) {
-	   Scanner input = new Scanner(System.in);
-	   System.out.print("Enter number of elements in the array: ");
-	   int number = input.nextInt();   
-	   int array[] = new int[number];
+   public static void main(String[] args) {
+      Scanner input = new Scanner(System.in);
+      System.out.print("Enter number of elements in the array: ");
+      int number = input.nextInt();   
+      int array[] = new int[number];
 		    
-	   System.out.print("\n");
-	   System.out.print("Please enter the actual values in the array: ");
-	   for(int i = 0; i < number; i++) {
-		 array[i] = input.nextInt(); 
-	   }
+      System.out.print("\n");
+      System.out.print("Please enter the actual values in the array: ");
+      for(int i = 0; i < number; i++) {
+        array[i] = input.nextInt(); 
+      }
 	   
-	   int max = lengthOfLIS(array);
+      int max = lengthOfLIS(array);
        
-       System.out.println();
-       System.out.println("The length of the Longest Increasing Subsequence is: " + max);
+      System.out.println();
+      System.out.println("The length of the Longest Increasing Subsequence is: " + max);
    }
 }
