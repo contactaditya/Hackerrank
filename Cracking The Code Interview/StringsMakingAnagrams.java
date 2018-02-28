36080b114bafac1cb16909387565469d9eb24e6d
Aditya Gupta <contactadityag@gmail.com>
2018-02-28 09:47:50 -0500

    This is the solution to the Hackerrank Problem Strings: Making Anagrams.

--- Cracking The Code Interview/StringsMakingAnagrams.java
+++ Cracking The Code Interview/StringsMakingAnagrams.java
@@ -1,53 +1,53 @@
 import java.util.Scanner;
 
-public class StringsMakingAnagrams {
+ public class StringsMakingAnagrams {
  
- public static int NUMBER_LETTERS = 26;
+   public static int NUMBER_LETTERS = 26;
 
- public static int getDelta(int[] array1, int[] array2) {
-  if(array1.length != array2.length) {
-     return -1;  
-  }
+   public static int getDelta(int[] array1, int[] array2) {
+     if(array1.length != array2.length) {
+       return -1;  
+     }
   
-  int delta = 0;
+     int delta = 0;
   
-  for(int i = 0; i < array1.length; i++) {
-     int difference = Math.abs(array1[i] - array2[i]);
-     delta += difference;
-  }
+     for(int i = 0; i < array1.length; i++) {
+       int difference = Math.abs(array1[i] - array2[i]);
+       delta += difference;
+     }
 	 
- return delta;	 
- }
+     return delta;	 
+   }
 
- public static int[] getCharCounts(String string) {
+   public static int[] getCharCounts(String string) {
   
-  int[] charCounts = new int[NUMBER_LETTERS];	 
+     int[] charCounts = new int[NUMBER_LETTERS];	 
   
-  for(int i=0; i < string.length(); i++) {
-    char c = string.charAt(i);
-    int offset = (int) 'a';
-    int code = c - offset;
-    charCounts[code]++;
-  }
-  return charCounts;	 
- }
+     for(int i=0; i < string.length(); i++) {
+       char c = string.charAt(i);
+       int offset = (int) 'a';
+       int code = c - offset;
+       charCounts[code]++;
+     }
+     return charCounts;	 
+   }
 	
- public static int numberNeeded(String first, String second) {
-  int[] charCount1 = getCharCounts(first);	
-  int[] charCount2 = getCharCounts(second);		 
+   public static int numberNeeded(String first, String second) {
+     int[] charCount1 = getCharCounts(first);	
+     int[] charCount2 = getCharCounts(second);		 
  
-  return getDelta(charCount1, charCount2);      
- }
+     return getDelta(charCount1, charCount2);      
+   }
 
- public static void main(String[] args) {
-   Scanner input = new Scanner(System.in);
-   System.out.print("Enter the first string: ");
-   String first = input.next();
-   System.out.println();
-   System.out.print("Enter the second string: ");
-   String second = input.next();
-   System.out.println();
-   System.out.print("The number of characters that you need to delete to make the strings anagrams of each other: ");
-   System.out.println(numberNeeded(first, second));
+   public static void main(String[] args) {
+     Scanner input = new Scanner(System.in);
+     System.out.print("Enter the first string: ");
+     String first = input.next();
+     System.out.println();
+     System.out.print("Enter the second string: ");
+     String second = input.next();
+     System.out.println();
+     System.out.print("The number of characters that you need to delete to make the strings anagrams of each other: ");
+     System.out.println(numberNeeded(first, second));
+   }
  }
-}
