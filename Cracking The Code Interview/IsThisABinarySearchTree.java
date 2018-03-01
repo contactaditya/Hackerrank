3afd26553cc2f4c00fd6bb8d845236b5e70fea32
Aditya Gupta <contactadityag@gmail.com>
2018-03-01 13:48:22 -0500

    This is the solution to the Hackerrank Problem Trees: Is This a Binary Search Tree?.

--- Cracking The Code Interview/IsThisABinarySearchTree.java
+++ Cracking The Code Interview/IsThisABinarySearchTree.java
@@ -11,8 +11,7 @@ import java.util.*;
    } 
  }
  
- public class IsThisABinarySearchTree {
-	
+ public class IsThisABinarySearchTree {	
    Node root;	    
    Node previous;
   
@@ -20,8 +19,7 @@ import java.util.*;
      return checkBST(root, Integer.MIN_VALUE, Integer.MAX_VALUE); 
    }
  
-   boolean checkBST(Node node, int min, int max) {
-	 
+   boolean checkBST(Node node, int min, int max) { 
       if (node == null) {
         return true;
       }
@@ -30,24 +28,22 @@ import java.util.*;
 	return false; 
       }
     
-   return (checkBST(node.left, min, node.data-1) && checkBST(node.right, node.data+1, max));  
- }
+     return (checkBST(node.left, min, node.data-1) && checkBST(node.right, node.data+1, max));  
+   }
 	
- public static void main(String[] args) {
-	 
-    IsThisABinarySearchTree tree = new IsThisABinarySearchTree();
-    tree.root = new Node(4);
-    tree.root.left = new Node(2);
-    tree.root.right = new Node(5);
-    tree.root.left.left = new Node(1);
-    tree.root.left.right = new Node(3);
+   public static void main(String[] args) {	 
+     IsThisABinarySearchTree tree = new IsThisABinarySearchTree();
+     tree.root = new Node(4);
+     tree.root.left = new Node(2);
+     tree.root.right = new Node(5);
+     tree.root.left.left = new Node(1);
+     tree.root.left.right = new Node(3);
      
-    if (tree.checkBST()) {
-      System.out.println("This tree is a BST");
-    }
-    else {
-      System.out.println("This tree is not a BST");
-    }
-
-  }
-}
+     if (tree.checkBST()) {
+       System.out.println("This tree is a BST");
+     }
+     else {
+       System.out.println("This tree is not a BST");
+     }
+   }
+ }
