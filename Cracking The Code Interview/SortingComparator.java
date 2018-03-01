a07cfa124643d2db947d1b372b51198451a97616
Aditya Gupta <contactadityag@gmail.com>
2018-03-01 14:12:19 -0500

    This is the solution to the Hackerrank Problem Sorting: Comparator.

--- Cracking The Code Interview/SortingComparator.java
+++ Cracking The Code Interview/SortingComparator.java
@@ -2,48 +2,48 @@ import java.util.Arrays;
 import java.util.Comparator;
 import java.util.Scanner;
 
-class Checker implements Comparator<Player> {
+  class Checker implements Comparator<Player> {
 	   
- public int compare(Player a, Player b) {
-  if(a.score == b.score) {
-    return a.name.compareTo(b.name);
+    public int compare(Player a, Player b) {
+      if(a.score == b.score) {
+        return a.name.compareTo(b.name);
+      }
+      return b.score - a.score;
+    }
   }
-  return b.score - a.score;
+
+  class Player{
+    String name;
+    int score;
+
+    Player(String name, int score) {
+      this.name = name;
+      this.score = score;
+    }
   }
- }
 
-class Player{
-  String name;
-  int score;
+  public class SortingComparator {
 
-  Player(String name, int score) {
-    this.name = name;
-    this.score = score;
-  }
-}
+    public static void main(String[] args) {
+      Scanner input = new Scanner(System.in);
+      System.out.print("Enter number of players: ");
+      int n = input.nextInt();
 
-public class SortingComparator {
-
- public static void main(String[] args) {
-  Scanner input = new Scanner(System.in);
-  System.out.print("Enter number of players: ");
-  int n = input.nextInt();
-
-  Player[] player = new Player[n];
-  Checker checker = new Checker();
+      Player[] player = new Player[n];
+      Checker checker = new Checker();
   
-  System.out.println();
-  System.out.print("Enter the player's name and score: ");
+      System.out.println();
+      System.out.print("Enter the player's name and score: ");
 
-  for(int i = 0; i < n; i++){
-    player[i] = new Player(input.next(), input.nextInt());
-  }
-  input.close();
+      for(int i = 0; i < n; i++) {
+        player[i] = new Player(input.next(), input.nextInt());
+      } 
+      input.close();
 
-  Arrays.sort(player, checker);
-  System.out.println();
-  for(int i = 0; i < player.length; i++){
-    System.out.printf("%s %s\n", player[i].name, player[i].score);
+      Arrays.sort(player, checker);
+      System.out.println();
+      for(int i = 0; i < player.length; i++){
+        System.out.printf("%s %s\n", player[i].name, player[i].score);
+      }
+    }
   }
- }
-}
