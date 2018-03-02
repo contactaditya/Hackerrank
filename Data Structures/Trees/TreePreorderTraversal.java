bfc22f2591479b2071e60631f49752e94701966a
Aditya Gupta <contactadityag@gmail.com>
2018-03-02 03:54:10 -0500

    This is the solution to the Hackerrank Problem: Tree: Preorder Traversal.

--- Data Structures/Trees/TreePreorderTraversal.java
+++ Data Structures/Trees/TreePreorderTraversal.java
@@ -1,33 +1,31 @@
 import java.util.*;
 
- class Node {
-   int data;
-   Node left;
-   Node right;
+  class Node {
+    int data;
+    Node left;
+    Node right;
 	   
-  public Node(int item) {
-    data = item;
-    left = right = null;
-  }
- }
-
-public class TreePreorderTraversal {
-	
-  Node root;	    
-  Node previous;
-	
-  public void printPreorderTraversal(Node node) {
-		 
-    if (node == null) {
-	  return;
+    public Node(int item) {
+      data = item;
+      left = right = null;
     }
-    
-    System.out.print(node.data + " ");		  
-    printPreorderTraversal(node.left);
-    printPreorderTraversal(node.right);
   }
+
+  public class TreePreorderTraversal {	
+    Node root;	    
+    Node previous;
+	
+    public void printPreorderTraversal(Node node) {	 
+      if (node == null) {
+	return;
+      }
+    
+      System.out.print(node.data + " ");		  
+      printPreorderTraversal(node.left);
+      printPreorderTraversal(node.right);
+    }
 			
-  public static void main(String[] args) {  
+    public static void main(String[] args) {  
       TreePreorderTraversal tree = new TreePreorderTraversal();
       tree.root = new Node(3);
       tree.root.left = new Node(5);
@@ -38,5 +36,5 @@ public class TreePreorderTraversal {
 	     
       System.out.print("Preorder traversal of binary tree is: ");
       tree.printPreorderTraversal(tree.root);		
+    }
   }
-}
