e23fb0460c29efb57e384b079090aeff8838262a
Aditya Gupta <contactadityag@gmail.com>
2018-03-03 04:33:21 -0500

    This is the solution to the Hackerrank Problem: RunningTimeOfAlgorithms.

--- Algorithms/Sorting/RunningTimeOfAlgorithms.java
+++ Algorithms/Sorting/RunningTimeOfAlgorithms.java
@@ -1,33 +1,33 @@
 import java.util.Scanner;
 
-public class RunningTimeOfAlgorithms {
+ public class RunningTimeOfAlgorithms {
 
- public static void main(String[] args) {   
-  Scanner input = new Scanner(System.in);
-  System.out.print("Enter the number of integers in the array: ");
-  int s = input.nextInt();
-  int[] ar = new int[s];
-  System.out.println();
-  System.out.print("Enter the elements in the array: ");
-  for(int i=0;i<s;i++) {
-   ar[i]=input.nextInt(); 
-  }
+   public static void main(String[] args) {   
+     Scanner input = new Scanner(System.in);
+     System.out.print("Enter the number of integers in the array: ");
+     int s = input.nextInt();
+     int[] ar = new int[s];
+     System.out.println();
+     System.out.print("Enter the elements in the array: ");
+     for(int i=0;i<s;i++) {
+       ar[i]=input.nextInt(); 
+     }
     
-  int counter = 0;
-  int sum = 0;    
+     int counter = 0;
+     int sum = 0;    
        
-  for(int i =1; i < ar.length; i++) {
-   int valueToSort = ar[i];
-   int j = i;
+     for(int i =1; i < ar.length; i++) {
+       int valueToSort = ar[i];
+       int j = i;
 
-  while(j>0 && ar[j-1] > valueToSort) {
-   ar[j] = ar[j-1]; 
-   j--; 
-   counter++;  
-  }          
-  ar[j] = valueToSort;  
- } 
- System.out.println(); 
- System.out.print("The number of shifts it takes to sort the array is: " + counter);   
+       while(j>0 && ar[j-1] > valueToSort) {
+         ar[j] = ar[j-1]; 
+         j--; 
+         counter++;  
+       }          
+       ar[j] = valueToSort;  
+     } 
+     System.out.println(); 
+     System.out.print("The number of shifts it takes to sort the array is: " + counter);   
+   }
  }
-}
