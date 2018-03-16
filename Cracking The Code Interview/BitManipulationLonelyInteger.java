4f58ac9a8c4b78df3f217b56525d5165dae0e209
Aditya Gupta <contactadityag@gmail.com>
2018-03-15 23:45:46 -0400

    This is the solution to the Hackerrank Problem Bit Manipulation: Lonely Integer.

--- Cracking The Code Interview/BitManipulationLonelyInteger.java
+++ Cracking The Code Interview/BitManipulationLonelyInteger.java
@@ -1,30 +1,30 @@
 import java.util.*;
 
-public class BitManipulationLonelyInteger {
+ public class BitManipulationLonelyInteger {
 	
- public static int lonelyInteger(int[] array) {
-  int n = array.length;
-  int result = 0; 
+   public static int lonelyInteger(int[] array) {
+     int n = array.length;
+     int result = 0; 
    
-  for(int value : array) {
-    result = result ^ value;    
-  }   
- return result;
- }
-
- public static void main(String[] args) {
-   Scanner input = new Scanner(System.in);
-   System.out.print("Enter number of elements in the array: ");
-   int numbers = input.nextInt();   
-   int array[] = new int[numbers];
-	    
-   System.out.print("\n"); 
-   System.out.print("Please enter the actual values in the array: ");
-   for (int i = 0; i < numbers; i++) {
-     array[i] = input.nextInt(); 
+     for(int value : array) {
+       result = result ^ value;    
+     }   
+     return result;
    }
+
+   public static void main(String[] args) {
+     Scanner input = new Scanner(System.in);
+     System.out.print("Enter number of elements in the array: ");
+     int numbers = input.nextInt();   
+     int array[] = new int[numbers];
+	    
+     System.out.print("\n"); 
+     System.out.print("Please enter the actual values in the array: ");
+     for (int i = 0; i < numbers; i++) {
+       array[i] = input.nextInt(); 
+     }
    
-   System.out.println();
-   System.out.println("The lonely integer is: " + lonelyInteger(array));
+     System.out.println();
+     System.out.println("The lonely integer is: " + lonelyInteger(array));
+   }
  }
-}
