87af10d98d25cf6e179288746fb998d38213e83e
Aditya Gupta <contactadityag@gmail.com>
2018-03-21 22:24:10 -0400

    This is the solution to the Hackerrank Problem Queues: A Tale of Two Stacks.

--- Cracking The Code Interview/QueuesATaleofTwoStacks.java
+++ Cracking The Code Interview/QueuesATaleofTwoStacks.java
@@ -3,55 +3,53 @@ import java.io.*;
 
 public class QueuesATaleofTwoStacks {
 
- public static class MyQueue<T> {
-	 
-  private Stack<T> stackNewestOnTop = new Stack<T>();
-  private Stack<T> stackOldestOnTop = new Stack<T>();
+  public static class MyQueue<T> { 
+    private Stack<T> stackNewestOnTop = new Stack<T>();
+    private Stack<T> stackOldestOnTop = new Stack<T>();
  
-  public void enqueue(T value) { // Add item
-    stackNewestOnTop.push(value); 
-  }
+    public void enqueue(T value) { // Add item
+      stackNewestOnTop.push(value); 
+    }
  
-  public T peek() { // Get "oldest" item
-    shiftStacks();
-    return stackOldestOnTop.peek();  
-  }
+    public T peek() { // Get "oldest" item
+      shiftStacks();
+      return stackOldestOnTop.peek();  
+    }
  
-  public T dequeue() { // Get "oldest" item and remove it
-    shiftStacks(); 
-    return stackOldestOnTop.pop();	 	 
-  }
+    public T dequeue() { // Get "oldest" item and remove it
+      shiftStacks(); 
+      return stackOldestOnTop.pop();	 	 
+    }
  
-  private void shiftStacks() {
-   if(stackOldestOnTop.isEmpty()) {
-     while(!stackNewestOnTop.isEmpty()) {
-       stackOldestOnTop.push(stackNewestOnTop.pop());	
-     }
-   }
+    private void shiftStacks() {
+      if(stackOldestOnTop.isEmpty()) {
+        while(!stackNewestOnTop.isEmpty()) {
+          stackOldestOnTop.push(stackNewestOnTop.pop());	
+        }
+      }
+    }
   }
- }
 		
-public static void main(String[] args) {  
-	 
-   MyQueue<Integer> queue = new MyQueue<Integer>();
+  public static void main(String[] args) {  	 
+    MyQueue<Integer> queue = new MyQueue<Integer>(); 
 
-   Scanner scan = new Scanner(System.in);
-   System.out.print("Enter number of operations: ");
-   int n = scan.nextInt();
+    Scanner scan = new Scanner(System.in);
+    System.out.print("Enter number of operations: ");
+    int n = scan.nextInt();
 
-   for (int i = 0; i < n; i++) {
-     System.out.println();	 
-     System.out.print("Enter the query type and value: ");	 
-     int operation = scan.nextInt();
-     if (operation == 1) { // enqueue
-       queue.enqueue(scan.nextInt());
-     } else if (operation == 2) { // dequeue
-       queue.dequeue();
-     } else if (operation == 3) { // print/peek
-       System.out.println();	 
-       System.out.println(queue.peek());
-     }
-   }
-  scan.close();
- }
+    for (int i = 0; i < n; i++) {
+      System.out.println();	 
+      System.out.print("Enter the query type and value: ");	 
+      int operation = scan.nextInt();
+      if (operation == 1) { // enqueue
+        queue.enqueue(scan.nextInt());
+      } else if (operation == 2) { // dequeue
+        queue.dequeue();
+      } else if (operation == 3) { // print/peek
+        System.out.println();	 
+        System.out.println(queue.peek());
+      }
+    }
+    scan.close();
+  }
 }
