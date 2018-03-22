c54a48b3716bacc1154d5d216f735c3bfe050a00
Aditya Gupta <contactadityag@gmail.com>
2018-03-21 22:15:00 -0400

    This is the solution to the Hackerrank Problem Hash Tables: Ransom Note.

--- Cracking The Code Interview/HashTablesRansomNote.java
+++ Cracking The Code Interview/HashTablesRansomNote.java
@@ -3,7 +3,7 @@ import java.io.*;
 
 public class HashTablesRansomNote {     
 	
- public static String getResponse(int m, int n, String[] magazine, String[] ransom) {
+  public static String getResponse(int m, int n, String[] magazine, String[] ransom) {
     HashMap<String, Integer> magazineHashMap = new HashMap<String, Integer>();  
     HashMap<String, Integer> ransomHashMap = new HashMap<String, Integer>(); 
     int count = 0;
@@ -24,21 +24,21 @@ public class HashTablesRansomNote {
         response = "No";
       }
     }
-  return response;
- }
+    return response;
+  }
  
- public static HashMap<String, Integer> getStringFrequency(String[] text) {
-   HashMap<String, Integer> frequencies = new HashMap<String, Integer>();
-   for(String word : text) {
-     if(!frequencies.containsKey(word)) {
+  public static HashMap<String, Integer> getStringFrequency(String[] text) {
+    HashMap<String, Integer> frequencies = new HashMap<String, Integer>();
+    for(String word : text) {
+      if(!frequencies.containsKey(word)) {
         frequencies.put(word, 0); 
-     }
-    frequencies.put(word, frequencies.get(word) + 1);
-   }
-   return frequencies;
- }
+      }
+      frequencies.put(word, frequencies.get(word) + 1);
+    }
+    return frequencies;
+  }
  
- public static boolean hasEnoughStrings(HashMap<String, Integer> magazineFrequency, HashMap<String, Integer> ransomFrequency) {
+  public static boolean hasEnoughStrings(HashMap<String, Integer> magazineFrequency, HashMap<String, Integer> ransomFrequency) {
     HashMap<String, Integer> frequencies = new HashMap<String, Integer>();
     for(Map.Entry<String, Integer> entry : ransomFrequency.entrySet()) {
       String word = entry.getKey();   
@@ -46,17 +46,17 @@ public class HashTablesRansomNote {
         return false;	
       }  
     } 
-  return true;
- }
+    return true;
+  }
  
- public static boolean canBuildRandomNote(String[] magazine, String[] ransom) {
-   HashMap<String, Integer> magazineFrequency = getStringFrequency(magazine);  
-   HashMap<String, Integer> ransomFrequency = getStringFrequency(ransom);  
+  public static boolean canBuildRandomNote(String[] magazine, String[] ransom) {
+    HashMap<String, Integer> magazineFrequency = getStringFrequency(magazine);  
+    HashMap<String, Integer> ransomFrequency = getStringFrequency(ransom);  
 
-   return hasEnoughStrings(magazineFrequency, ransomFrequency);
- }
+    return hasEnoughStrings(magazineFrequency, ransomFrequency);
+  }
 		
- public static void main(String[] args) { 
+  public static void main(String[] args) { 
     Scanner input = new Scanner(System.in);
     System.out.print("Enter number of words in magazine and ransom note: ");
     int m = input.nextInt();
@@ -79,5 +79,5 @@ public class HashTablesRansomNote {
   
     System.out.println();
     System.out.println(canBuildRandomNote(magazine,ransom));  
- }
+  }
 }
