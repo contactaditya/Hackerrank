58bba1a57136b28828d891ab413e013ba5ee212f
Aditya Gupta <contactadityag@gmail.com>
2018-03-24 02:51:55 -0400

    This is the solution to the Hackerrank Problem: Tree: Inorder Traversal.

--- Data Structures/Trees/TreeInorderTraversal.java
+++ Data Structures/Trees/TreeInorderTraversal.java
@@ -4,27 +4,26 @@ import java.util.*;
    Node root;	    
    Node previous;
 
- public void printInorderTraversal(Node node) {
-		 
-    if (node == null) {
+   public void printInorderTraversal(Node node) {	 
+     if (node == null) {
        return;
-    }
+     }
  
-    printInorderTraversal(node.left);
-    System.out.print(node.data + " ");	
-    printInorderTraversal(node.right);
- }
+     printInorderTraversal(node.left);
+     System.out.print(node.data + " ");	
+     printInorderTraversal(node.right);
+   }
 			
- public static void main(String[] args) {  
-      TreeInorderTraversal tree = new TreeInorderTraversal();
-      tree.root = new Node(3);
-      tree.root.left = new Node(5);
-      tree.root.right = new Node(2);
-      tree.root.left.left = new Node(1);
-      tree.root.left.right = new Node(4);
-      tree.root.right.left = new Node(6);
+   public static void main(String[] args) {  
+     TreeInorderTraversal tree = new TreeInorderTraversal();
+     tree.root = new Node(3);
+     tree.root.left = new Node(5);
+     tree.root.right = new Node(2);
+     tree.root.left.left = new Node(1);
+     tree.root.left.right = new Node(4);
+     tree.root.right.left = new Node(6);
 	     
-      System.out.print("Inorder traversal of binary tree is: ");
-      tree.printInorderTraversal(tree.root);		
+     System.out.print("Inorder traversal of binary tree is: ");
+     tree.printInorderTraversal(tree.root);		
+   }
  }
-}
