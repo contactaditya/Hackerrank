de74631a34bd03be3bdc942971367ef64e8f204e
Aditya Gupta <contactadityag@gmail.com>
2018-03-24 02:23:39 -0400

    This is the solution to the Hackerrank Problem: Mini-Max Sum.

--- Algorithms/Warmup/MiniMaxSum.java
+++ Algorithms/Warmup/MiniMaxSum.java
@@ -3,30 +3,30 @@ import java.util.Scanner;
 
   public class MiniMaxSum {
 
-	public static void main(String[] args) {
-	   Scanner input = new Scanner(System.in);
-	   long array[] = new long[5];
-	   System.out.print("Please enter only five values in the array: ");
-	   for (int i = 0; i < 5; i++) {
-		  array[i] = input.nextLong(); 
-	   }
+     public static void main(String[] args) {
+       Scanner input = new Scanner(System.in);
+       long array[] = new long[5];
+       System.out.print("Please enter only five values in the array: ");
+       for (int i = 0; i < 5; i++) {
+	 array[i] = input.nextLong(); 
+       }
 	   
-	   Arrays.sort(array);
-	   long min = 0;
-	   long max = 0;
+       Arrays.sort(array);
+       long min = 0;
+       long max = 0;
 	   
-	   for(int i=0; i<4; i++) {
-		  min += array[i];
-	   }
+       for(int i=0; i<4; i++) {
+	 min += array[i];
+       }
 	   
-	   for(int i=1; i<5; i++) {
-		  max += array[i];
-	   }
+       for(int i=1; i<5; i++) {
+	 max += array[i];
+       }
 	   
-	   System.out.println();
-	   System.out.println("The minimum value that can be obtained by summing exactly four of the five integers: " + min);
+       System.out.println();
+       System.out.println("The minimum value that can be obtained by summing exactly four of the five integers: " + min);
 	   
-	   System.out.println();
-	   System.out.println("The maximum value that can be obtained by summing exactly four of the five integers: " + max);	   
-	}
- }
+       System.out.println();
+       System.out.println("The maximum value that can be obtained by summing exactly four of the five integers: " + max);	   
+     }
+  }
