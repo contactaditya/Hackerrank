3485f3124429e51bd8f941e8b4c7ce0c0e5887c6
Aditya Gupta <contactadityag@gmail.com>
2018-03-23 23:18:26 -0400

    This is the solution to the Hackerrank Problem: Quicksort 1 - Partition.

--- Algorithms/Sorting/Quicksort1Partition.java
+++ Algorithms/Sorting/Quicksort1Partition.java
@@ -10,29 +10,27 @@ public class Quicksort1Partition {
     List<Integer> leftElements = new ArrayList<Integer>();
     List<Integer> rightElements = new ArrayList<Integer>();    
         
-    for(int i = 1; i < array.length; i++) {
-          
-     if(array[i] < pivot) {
-       leftElements.add(array[i]);
-     }
-          
-     else {
-       rightElements.add(array[i]); 
-     }
+    for(int i = 1; i < array.length; i++) {        
+      if(array[i] < pivot) {
+        leftElements.add(array[i]);
+      }
+      else {
+        rightElements.add(array[i]); 
+      }
     }
           
-     for (int i = 0; i < leftElements.size(); i++) {
-       array[i] = leftElements.get(i);
-     }  
+    for (int i = 0; i < leftElements.size(); i++) {
+      array[i] = leftElements.get(i);
+    }  
           
-     array[leftElements.size()] = pivot;  
+    array[leftElements.size()] = pivot;  
           
-     for(int i = 0; i < rightElements.size(); i++) {
-	array[i + leftElements.size() + 1] = rightElements.get(i);
-     }  
+    for(int i = 0; i < rightElements.size(); i++) {
+      array[i + leftElements.size() + 1] = rightElements.get(i);
+    }  
           
-  printArray(array);               
- }   
+    printArray(array);               
+  }   
  
   static void printArray(int[] array) {
     System.out.print("\n"); 
@@ -44,16 +42,16 @@ public class Quicksort1Partition {
   }
 
   public static void main(String[] args) {
-     Scanner input = new Scanner(System.in);
-     System.out.print("Enter number of elements in the array: ");
-     int numbers = input.nextInt();
-     int[] array = new int[numbers];
-     System.out.print("\n");
-     System.out.print("Please enter the actual values in the array: ");
-     for(int i=0; i<numbers; i++) {
-       array[i] = input.nextInt();
-     }
-     partition(array);
+    Scanner input = new Scanner(System.in);
+    System.out.print("Enter number of elements in the array: ");
+    int numbers = input.nextInt();
+    int[] array = new int[numbers];
+    System.out.print("\n");
+    System.out.print("Please enter the actual values in the array: ");
+    for(int i=0; i<numbers; i++) {
+      array[i] = input.nextInt();
+    }
+    partition(array);
   }
- }
+}
 
