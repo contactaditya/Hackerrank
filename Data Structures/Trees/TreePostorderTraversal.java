28d2280090e572b8d608b866ce62b283814faf69
Aditya Gupta <contactadityag@gmail.com>
2018-03-24 03:04:31 -0400

    This is the solution to the Hackerrank Problem: Tree: Postorder Traversal.

--- Data Structures/Trees/TreePostorderTraversal.java
+++ Data Structures/Trees/TreePostorderTraversal.java
@@ -4,27 +4,26 @@ import java.util.*;
    Node root;	    
    Node previous;
 
-  public void printPostorderTraversal(Node node) {
-		 
-    if (node == null) {
+   public void printPostorderTraversal(Node node) { 
+     if (node == null) {
        return;
-    }
+     }
    
-    printPostorderTraversal(node.left);
-    printPostorderTraversal(node.right);
-    System.out.print(node.data + " ");		
- }
+     printPostorderTraversal(node.left);
+     printPostorderTraversal(node.right);
+     System.out.print(node.data + " ");		
+   }
 			
- public static void main(String[] args) {  
-    TreePostorderTraversal tree = new TreePostorderTraversal();
-    tree.root = new Node(3);
-    tree.root.left = new Node(5);
-    tree.root.right = new Node(2);
-    tree.root.left.left = new Node(1);
-    tree.root.left.right = new Node(4);
-    tree.root.right.left = new Node(6);
+   public static void main(String[] args) {  
+     TreePostorderTraversal tree = new TreePostorderTraversal();
+     tree.root = new Node(3);
+     tree.root.left = new Node(5);
+     tree.root.right = new Node(2);
+     tree.root.left.left = new Node(1);
+     tree.root.left.right = new Node(4);
+     tree.root.right.left = new Node(6);
 	     
-    System.out.print("Postorder traversal of binary tree is: ");
-    tree.printPostorderTraversal(tree.root);		
+     System.out.print("Postorder traversal of binary tree is: ");
+     tree.printPostorderTraversal(tree.root);		
+   }
  }
-}
