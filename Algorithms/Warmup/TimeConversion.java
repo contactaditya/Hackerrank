4429aec31e148b7aa08f9be7e194915a37056f18
Aditya Gupta <contactadityag@gmail.com>
2018-03-24 02:36:48 -0400

    This is the solution to the Hackerrank Problem: Time Conversion.

--- Algorithms/Warmup/TimeConversion.java
+++ Algorithms/Warmup/TimeConversion.java
@@ -1,30 +1,30 @@
 import java.util.Scanner;
 
-public class TimeConversion {
+ public class TimeConversion {
 
- public static void main(String[] args) {  
-  Scanner input = new Scanner(System.in);
-  System.out.print("Enter the time in AM/PM format: ");
-  String inputTime = input.next();
-  String [] timeArray = inputTime.split(":");
-  String hours = timeArray[0];
-  String minutes = timeArray[1];
-  String seconds = timeArray[2].substring(0, 2);
+   public static void main(String[] args) {  
+     Scanner input = new Scanner(System.in);
+     System.out.print("Enter the time in AM/PM format: ");
+     String inputTime = input.next();
+     String [] timeArray = inputTime.split(":");
+     String hours = timeArray[0];
+     String minutes = timeArray[1];
+     String seconds = timeArray[2].substring(0, 2);
     
- if(timeArray[2].substring(2,4).equalsIgnoreCase("PM")) {
-   if (Integer.parseInt(hours) < 12) {
-    int tempHours = Integer.parseInt(hours);
-    tempHours += 12;
-    hours = Integer.toString(tempHours);
-   }     
- }
+     if(timeArray[2].substring(2,4).equalsIgnoreCase("PM")) {
+       if (Integer.parseInt(hours) < 12) {
+         int tempHours = Integer.parseInt(hours);
+         tempHours += 12;
+         hours = Integer.toString(tempHours);
+       }     
+     }
     
- if(timeArray[2].substring(2,4).equalsIgnoreCase("AM")) {
-   if (Integer.parseInt(hours) == 12) {
-     hours = "00";
-   }       
+     if(timeArray[2].substring(2,4).equalsIgnoreCase("AM")) {
+       if (Integer.parseInt(hours) == 12) {
+         hours = "00";
+       }       
+     }
+     System.out.println();
+     System.out.println("The time in 24-hour format is: " + hours + ":" + minutes + ":" + seconds);
+   }
  }
- System.out.println();
- System.out.println("The time in 24-hour format is: " + hours + ":" + minutes + ":" + seconds);
- }
-}
