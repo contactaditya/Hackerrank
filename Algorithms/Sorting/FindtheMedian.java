9a8ef5e28543f5344bf630d1bc274276ff22dee6
Aditya Gupta <contactadityag@gmail.com>
2018-03-24 00:28:31 -0400

    This is the solution to the Hackerrank Problem: Find the Median.

--- Algorithms/Sorting/FindtheMedian.java
+++ Algorithms/Sorting/FindtheMedian.java
@@ -3,55 +3,48 @@ import java.util.*;
  
  public class FindtheMedian {
 	 
-	public static long calculateMedian(long[] array) {
-	   int n = array.length;  
-	   long median = 0;
-				     
-	   if (n%2!=0) {
-		  median = array[n/2];
-	   }
-
-	   if (n%2==0) {
-		  median = (array[n/2] + array[(n-2)/2])/2;
-	   }
-
+     public static long calculateMedian(long[] array) {
+       int n = array.length;  
+       long median = 0;			     
+       if (n %2 != 0) {
+	 median = array[n/2];
+       }
+       if (n % 2 == 0) {
+	 median = (array[n/2] + array[(n-2)/2])/2;
+       }
        return median;
-	}	
+     }	
 
-	public static void main(String[] args) {
-	 	Scanner input = new Scanner(System.in);
-	    System.out.print("Enter number of elements in the array: ");
-	    int numbers = input.nextInt();   
-	    long array[] = new long[numbers];
+     public static void main(String[] args) {
+       Scanner input = new Scanner(System.in);
+       System.out.print("Enter number of elements in the array: ");
+       int numbers = input.nextInt();   
+       long array[] = new long[numbers];
 	    
-	    System.out.print("\n");
-		System.out.print("Please enter the actual values in the array: ");
-		for (int i = 0; i < numbers; i++) {
-		  array[i] = input.nextInt(); 
-		}
+       System.out.print("\n");
+       System.out.print("Please enter the actual values in the array: ");
+       for (int i = 0; i < numbers; i++) {
+	 array[i] = input.nextInt(); 
+       }
 		
-		System.out.print("\n");
+       System.out.print("\n");
+       System.out.print("The actual values in the array are: ");
+       for (int i = 0; i < numbers; i++) {
+	 System.out.print(array[i] + " ");
+       }  
 		
-		System.out.print("The actual values in the array are: ");
-		for (int i = 0; i < numbers; i++) {
-		  System.out.print(array[i] + " ");
-		}  
+       System.out.print("\n \n");
+       Arrays.sort(array);
+       System.out.print("The sorted values in the array are: ");
 		
-		System.out.print("\n \n");
-		
-		Arrays.sort(array);
-		
-		System.out.print("The sorted values in the array are: ");
-		
-		 for (Long i : array) {
-		   System.out.print(i.intValue() + " ");
-		 }
+       for (Long i : array) {
+	 System.out.print(i.intValue() + " ");
+       }
 		  	
-	    long median = calculateMedian(array);
+       long median = calculateMedian(array);
 	    
-	    System.out.print("\n \n");
+       System.out.print("\n \n");
 	    
-	    System.out.println("The value of the median of the array is: " + median);
-
-	  }
-	}
+       System.out.println("The value of the median of the array is: " + median);
+     }
+ }
