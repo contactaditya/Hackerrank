04902a29114591632c79d1c16a8bcafee30c09c6
Aditya Gupta <contactadityag@gmail.com>
2018-04-21 18:43:23 -0400

    This is the solution to the Hackerrank Problem: Birthday Cake Candles.

--- Algorithms/Warmup/BirthdayCakeCandles.java
+++ Algorithms/Warmup/BirthdayCakeCandles.java
@@ -1,40 +1,40 @@
 import java.util.*;
 import java.io.*;
 
- public class BirthdayCakeCandles {
+  public class BirthdayCakeCandles {
 	 
-	public static int birthdayCakeCandles(int number, int array[]) {
-	  int frequency = 0;	
-	  int tallest = 0;
-	  for(int i=0; i < number; i++){
-	    int height = array[i];
+    public static int birthdayCakeCandles(int number, int array[]) {
+      int frequency = 0;	
+      int tallest = 0;
+      for(int i=0; i < number; i++) {
+	int height = array[i];
 	            
-	     if(height > tallest){
-	       tallest = height;
-	       frequency = 1;
-	     }
-	     else if(height == tallest) {
-	       frequency++;
-	     }
-	  }
-	   return frequency;
+	if(height > tallest) {
+	  tallest = height;
+	  frequency = 1;
 	}
+	else if(height == tallest) {
+	  frequency++;
+	}
+      }
+      return frequency;
+    }
 
-	public static void main(String[] args) {
-	   Scanner input = new Scanner(System.in);
-	   System.out.print("Enter number of elements in the array: ");
-	   int numbers = input.nextInt();   
-	   int array[] = new int[numbers];
+    public static void main(String[] args) {
+      Scanner input = new Scanner(System.in);
+      System.out.print("Enter number of elements in the array: "); 
+      int numbers = input.nextInt();   
+      int array[] = new int[numbers];
 	    
-	   System.out.print("\n");
-	   System.out.print("Please enter the actual values in the array: ");
-	   for(int i = 0; i < numbers; i++) {
-		 array[i] = input.nextInt(); 
-       }	
+      System.out.print("\n");
+      System.out.print("Please enter the actual values in the array: ");
+      for(int i = 0; i < numbers; i++) {
+	array[i] = input.nextInt(); 
+      }	
 	  
-	   int numberOfCandles = birthdayCakeCandles(numbers, array);
+      int numberOfCandles = birthdayCakeCandles(numbers, array);
 	    
-	   System.out.println();
-	   System.out.println("The number of candles she can successfully blow out are: " + numberOfCandles);
-	}
- }
+      System.out.println();
+      System.out.println("The number of candles she can successfully blow out are: " + numberOfCandles);
+    }
+  }
